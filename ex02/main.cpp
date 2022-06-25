d930783e5b7aa75b0ba3057773aa1b1a7ebbb275
Vinicius Cordeiro <viniciuscordeirop@gmail.com>
2022-06-25 20:29:02 -0300

    Update ex02 codestyle

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,17 +5,23 @@
 
 int main( void )
 {
-	std::string lString = "HI THIS IS BRAIN";
-	std::string* lpStringPTR = &lString;
-	std::string& lrStringREF = lString;
+    std::string lString = "HI THIS IS BRAIN";
+    std::string* lpStringPTR = &lString;
+    std::string& lrStringREF = lString;
 
-	std::cout << "The memory address of the string variable: " << &lString << std::endl;
-	std::cout << "The memory address held by stringPTR:      " << lpStringPTR << std::endl;
-	std::cout << "The memory address held by stringREF:      " << &lrStringREF << std::endl;
+    std::cout << std::endl;
 
-	std::cout << "The value of the string variable:  " << lString << std::endl;
-	std::cout << "The value pointed to by stringPTR: " << *lpStringPTR << std::endl;
-	std::cout << "The value pointed to by stringREF: " << lrStringREF << std::endl;
+    std::cout << "The memory address of the string variable: " << &lString << std::endl;
+    std::cout << "The memory address held by stringPTR:      " << lpStringPTR << std::endl;
+    std::cout << "The memory address held by stringREF:      " << &lrStringREF << std::endl;
 
-	return 0;
+    std::cout << std::endl;
+
+    std::cout << "The value of the string variable:  " << lString << std::endl;
+    std::cout << "The value pointed to by stringPTR: " << *lpStringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF: " << lrStringREF << std::endl;
+
+    std::cout << std::endl;
+
+    return 0;
 }
